f9ceb4903558363c4b2ecb1783ab18dc593bd4b4
evpobr <evpobr@gmail.com>
2020-02-10 14:42:50 +0500

    Rustify host_write_i2f() function

--- src/float32.c
+++ src/float32.c
@@ -60,6 +60,7 @@ RUST_EXTERN sf_count_t	host_read_f	(SF_PRIVATE *psf, float *ptr, sf_count_t len)
 RUST_EXTERN sf_count_t	host_read_f2d	(SF_PRIVATE *psf, double *ptr, sf_count_t len) ;
 
 RUST_EXTERN sf_count_t	host_write_s2f	(SF_PRIVATE *psf, const short *ptr, sf_count_t len) ;
+RUST_EXTERN sf_count_t	host_write_i2f	(SF_PRIVATE *psf, const int *ptr, sf_count_t len) ;
 
 RUST_EXTERN void		float32_peak_update	(SF_PRIVATE *psf, const float *buffer, int count, sf_count_t indx) ;
 #else
@@ -69,11 +70,11 @@ static sf_count_t	host_read_f	(SF_PRIVATE *psf, float *ptr, sf_count_t len) ;
 static sf_count_t	host_read_f2d	(SF_PRIVATE *psf, double *ptr, sf_count_t len) ;
 
 static sf_count_t	host_write_s2f	(SF_PRIVATE *psf, const short *ptr, sf_count_t len) ;
+static sf_count_t	host_write_i2f	(SF_PRIVATE *psf, const int *ptr, sf_count_t len) ;
 
 static void		float32_peak_update	(SF_PRIVATE *psf, const float *buffer, int count, sf_count_t indx) ;
 #endif
 
-static sf_count_t	host_write_i2f	(SF_PRIVATE *psf, const int *ptr, sf_count_t len) ;
 static sf_count_t	host_write_f	(SF_PRIVATE *psf, const float *ptr, sf_count_t len) ;
 static sf_count_t	host_write_d2f	(SF_PRIVATE *psf, const double *ptr, sf_count_t len) ;
 
@@ -745,8 +746,6 @@ host_write_s2f	(SF_PRIVATE *psf, const short *ptr, sf_count_t len)
 	return total ;
 } /* host_write_s2f */
 
-#endif
-
 static sf_count_t
 host_write_i2f	(SF_PRIVATE *psf, const int *ptr, sf_count_t len)
 {	BUF_UNION	ubuf ;
@@ -777,6 +776,8 @@ host_write_i2f	(SF_PRIVATE *psf, const int *ptr, sf_count_t len)
 
 	return total ;
 } /* host_write_i2f */
+
+#endif
 
 static sf_count_t
 host_write_f	(SF_PRIVATE *psf, const float *ptr, sf_count_t len)
