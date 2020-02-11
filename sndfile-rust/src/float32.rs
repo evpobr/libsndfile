9006590387c23b9cbf23e590a05bd7bd95238b53
evpobr <evpobr@gmail.com>
2020-02-11 15:27:57 +0500

    Rustify replace_read_f2d() function

--- sndfile-rust/src/float32.rs
+++ sndfile-rust/src/float32.rs
@@ -911,6 +911,58 @@ unsafe extern "C" fn replace_read_f(
 }
 
 #[no_mangle]
+unsafe extern "C" fn replace_read_f2d(
+    psf: *mut SF_PRIVATE,
+    ptr: *mut c_double,
+    len: sf_count_t,
+) -> sf_count_t {
+    assert!(len >= 0);
+    assert_ne!(ptr.is_null(), true);
+    assert_ne!(psf.is_null(), true);
+
+    let mut len = len as usize;
+    let ptr = slice::from_raw_parts_mut(ptr, len);
+    let psf = &mut *psf;
+
+    let mut ubuf = BUF_UNION {
+        fbuf: [0.0; SF_BUFFER_LEN / mem::size_of::<c_float>()],
+    };
+    let mut bufferlen = ubuf.fbuf.len();
+    let mut total = 0;
+
+    while len > 0 {
+        if len < bufferlen {
+            bufferlen = len;
+        }
+        let readcount = psf_fread(
+            ubuf.fbuf.as_mut_ptr() as *mut c_void,
+            mem::size_of::<c_float>() as sf_count_t,
+            bufferlen as sf_count_t,
+            psf,
+        ) as usize;
+
+        if psf.data_endswap == SF_TRUE {
+            endswap_int_array(ubuf.ibuf.as_mut_ptr(), bufferlen as c_int);
+        }
+
+        bf2f_array(ubuf.fbuf.as_mut_ptr(), bufferlen as c_int);
+
+        f2d_array(
+            ubuf.fbuf.as_mut_ptr(),
+            readcount as c_int,
+            ptr[total..].as_mut_ptr(),
+        );
+        total += readcount;
+        if readcount < bufferlen {
+            break;
+        }
+        len -= readcount;
+    }
+
+    return total as sf_count_t;
+}
+
+#[no_mangle]
 unsafe extern "C" fn bf2f_array(buffer: *mut c_float, count: c_int) {
     assert!(count >= 0);
     assert_ne!(buffer.is_null(), true);
